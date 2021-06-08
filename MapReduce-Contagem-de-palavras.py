2fd336810ce68bae9d78a0d069a433fa7eb4a53f
Felipenuns <85532552+Felipenuns@users.noreply.github.com>
2021-06-07 22:24:33 -0300

    Add files via upload

---
+++ MapReduce-Contagem-de-palavras.py
@@ -0,0 +1,42 @@
+#-*- coding: iso8859-1 -*-
+
+import mincemeat
+import glob
+import csv
+
+text_files = glob.glob('C:\\Windows\\Temp\\Exec\\Entrega final Concluido. 100%\\data\\contagem-de-palavras\\*');
+
+def file_contents(file_name):
+    f = open(file_name)
+    try:
+        return f.read()
+    finally:
+        f.close()
+
+source = dict((file_name, file_contents(file_name)) for file_name in text_files)
+
+def mapfn(k,v):
+    print ('map'+ k)
+    from stopwords import allStopWords
+    for line in v.splitlines():
+        for word in line.split():
+            if(word not in allStopWords):
+                yield word, 1
+
+def reducefn(k,v):
+    print ('reduce '+k)
+    return sum(v)
+
+source = dict((file_name,file_contents(file_name))for file_name in text_files)
+
+
+s = mincemeat.Server()
+s.datasource = source
+s.mapfn = mapfn
+s.reducefn = reducefn
+
+results = s.run_server(password='changeme')
+
+w = csv.writer(open("C:\\Windows\\Temp\\Exec\\RES1.csv","w"))
+for k, v in results.items():
+    w.writerow([k,v])
